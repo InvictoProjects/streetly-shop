81b0ee3fe531ff981bfd7d39d6da7e629c9c773f
Koroliuk <koroliuk.yaroslav@gmail.com>
2023-11-18 03:14:15 +0200

    Implement refresh endpoint

--- src/main/kotlin/com/invoctoprojects/streetlyshop/web/controller/AuthenticationController.kt
+++ src/main/kotlin/com/invoctoprojects/streetlyshop/web/controller/AuthenticationController.kt
@@ -4,6 +4,7 @@ import com.invoctoprojects.streetlyshop.service.LoginService
 import com.invoctoprojects.streetlyshop.web.controller.request.BasicLoginRequest
 import io.swagger.annotations.Api
 import io.swagger.annotations.ApiOperation
+import org.bson.types.ObjectId
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.*
 import javax.validation.Valid
@@ -18,4 +19,8 @@ class AuthenticationController(val loginService: LoginService) {
     @PostMapping("basic-login")
     fun basicLogin(@Valid @RequestBody loginRequest: BasicLoginRequest) = loginService.login(loginRequest)
 
+    @ApiOperation("Get access token using refresh token")
+    @PostMapping("refresh")
+    fun refresh(@CookieValue(name = "refreshToken") refreshToken: String, @RequestParam userId: String) =
+        loginService.refresh(ObjectId(userId), refreshToken)
 }
