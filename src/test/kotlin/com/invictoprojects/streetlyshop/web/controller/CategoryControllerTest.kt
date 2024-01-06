720440117baf01707c8e00e3c8c9efb1d477e556
Volodymyr Pienskoi <vladymyrpenskoy@gmail.com>
2024-01-07 01:42:30 +0200

    Imports must be ordered in lexicographic order

--- src/test/kotlin/com/invictoprojects/streetlyshop/web/controller/CategoryControllerTest.kt
+++ src/test/kotlin/com/invictoprojects/streetlyshop/web/controller/CategoryControllerTest.kt
@@ -21,9 +21,9 @@ import org.mockito.Mockito.verifyNoInteractions
 import org.mockito.junit.jupiter.MockitoExtension
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
 import java.time.Instant
