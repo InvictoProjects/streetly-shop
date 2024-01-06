f8b011aa938e28e04731263172cb4055fa4ac158
Vladyslav <proxima2002vt@gmail.com>
2024-01-06 21:49:35 +0200

    Add OrderDTO and OrderLineDTO classes

---
+++ src/main/kotlin/com/invictoprojects/streetlyshop/web/controller/dto/OrderDTO.kt
@@ -0,0 +1,20 @@
+package com.invictoprojects.streetlyshop.web.controller.dto
+
+import com.invictoprojects.streetlyshop.persistence.domain.model.order.OrderStatus
+import java.time.Instant
+
+data class OrderDTO(
+        val id: String,
+        val customerId: String,
+        val customer: CustomerDTO,
+        val creationDate: Instant,
+        val modifiedDate: Instant,
+        val deliveryService: String,
+        val city: String,
+        val department: String,
+        val recipientName: String,
+        val recipientSurname: String,
+        val recipientMiddleName: String,
+        val status: OrderStatus,
+        val lines: List<OrderLineDTO>
+)
