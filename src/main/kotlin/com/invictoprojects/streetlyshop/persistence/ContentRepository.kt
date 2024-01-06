fa3d7ddcf76cc8b983aa26a6f016610191dc7d71
Vladyslav <proxima2002vt@gmail.com>
2024-01-06 20:22:52 +0200

    Add new methods to ContentRepository

--- src/main/kotlin/com/invictoprojects/streetlyshop/persistence/ContentRepository.kt
+++ src/main/kotlin/com/invictoprojects/streetlyshop/persistence/ContentRepository.kt
@@ -6,6 +6,10 @@ import org.bson.types.ObjectId
 
 interface ContentRepository {
     fun save(content: Content): Content
+    fun findByIdAggregated(id: ObjectId, language: Language): Content?
     fun getByIdAggregated(id: ObjectId, language: Language): Content
+    fun existsById(id: ObjectId, language: Language): Boolean
+    fun findById(id: ObjectId, language: Language): Content?
     fun getById(id: ObjectId, language: Language): Content
+    fun getCreator(id: ObjectId, language: Language): ObjectId
 }
