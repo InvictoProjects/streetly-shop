49e5723cf5cb25d2845a96cea7b8351a02be3e28
Vladyslav <proxima2002vt@gmail.com>
2024-01-06 20:16:23 +0200

    Add new request classes for creating and updating content

---
+++ src/main/kotlin/com/invictoprojects/streetlyshop/web/controller/request/CreateContentRequest.kt
@@ -0,0 +1,16 @@
+package com.invictoprojects.streetlyshop.web.controller.request
+
+import com.invictoprojects.streetlyshop.web.controller.dto.AttributeDTO
+import javax.validation.constraints.NotBlank
+import javax.validation.constraints.Size
+
+data class CreateContentRequest(
+        @field:NotBlank
+        val productId: String? = null,
+        @field:NotBlank
+        val name: String? = null,
+        @field:NotBlank
+        val description: String? = null,
+        @field:Size(min = 1)
+        val attributes: List<AttributeDTO> = emptyList()
+)
