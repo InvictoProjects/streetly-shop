c1867d09abfa5b8c7bfb7daa168cb69def29abe4
Koroliuk <koroliuk.yaroslav@gmail.com>
2024-01-06 20:15:13 +0200

    Update customer service

--- src/main/kotlin/com/invictoprojects/streetlyshop/service/CustomerService.kt
+++ src/main/kotlin/com/invictoprojects/streetlyshop/service/CustomerService.kt
@@ -1,9 +1,13 @@
 package com.invictoprojects.streetlyshop.service
 
 import com.invictoprojects.streetlyshop.persistence.CustomerRepository
+import com.invictoprojects.streetlyshop.persistence.ProductRepository
 import com.invictoprojects.streetlyshop.persistence.domain.model.customer.Customer
 import com.invictoprojects.streetlyshop.persistence.domain.model.customer.Role
+import com.invictoprojects.streetlyshop.persistence.impl.toObjectId
+import com.invictoprojects.streetlyshop.web.controller.dto.CustomerDTO
 import com.invictoprojects.streetlyshop.web.controller.request.UpdateCustomerDetailsRequest
+import com.invictoprojects.streetlyshop.web.controller.response.ImageUploadResponse
 import com.invictoprojects.streetlyshop.web.exception.UserAlreadyRegisteredException
 import org.bson.types.ObjectId
 import org.springframework.security.core.authority.SimpleGrantedAuthority
@@ -12,11 +16,17 @@ import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
+import org.springframework.web.multipart.MultipartFile
+
+const val AVATARS_CONTAINER = "avatars"
 
 @Service
 class CustomerService(
     val customerRepository: CustomerRepository,
+    val productRepository: ProductRepository,
     val passwordEncoder: PasswordEncoder,
+    val imageService: ImageService,
+    val fileService: FileService
 ) : UserDetailsService {
 
     override fun loadUserByUsername(email: String): UserDetails {
@@ -27,6 +37,22 @@ class CustomerService(
     fun register(email: String, rawPassword: String) {
         if (customerRepository.findByEmail(email) != null) throw UserAlreadyRegisteredException(email)
         customerRepository.save(getCustomer(email, rawPassword))
+    }
+
+    fun updateAvatar(file: MultipartFile, userId: ObjectId): ImageUploadResponse {
+        val customer = customerRepository.getById(userId)
+
+        val oldAvatarFileName = customer.getAvatarFileName()
+        val newAvatar = imageService.toAvatar(file, userId)
+        val newAvatarUrl = fileService.uploadFile(newAvatar, AVATARS_CONTAINER)
+
+        customer.avatar = newAvatarUrl
+        customerRepository.save(customer)
+
+        if (hasFileNameChanged(oldAvatarFileName, newAvatar.name)) {
+            fileService.deleteFile(oldAvatarFileName!!, AVATARS_CONTAINER)
+        }
+        return ImageUploadResponse(newAvatarUrl)
     }
 
     fun updateDetails(userId: ObjectId, updateRequest: UpdateCustomerDetailsRequest) {
@@ -45,6 +71,10 @@ class CustomerService(
         customerRepository.save(customer)
     }
 
+    private fun hasFileNameChanged(oldAvatarFileName: String?, newAvatarFileName: String): Boolean {
+        return oldAvatarFileName != null && oldAvatarFileName != newAvatarFileName
+    }
+
     private fun getCustomer(email: String, rawPassword: String) =
         Customer(
             email = email,
@@ -52,4 +82,39 @@ class CustomerService(
             password = passwordEncoder.encode(rawPassword),
             roles = mutableListOf(Role.BUYER, Role.SELLER)
         )
+
+    fun addFavoriteProduct(userId: String, productIdString: String) {
+        val user = customerRepository.getById(userId.toObjectId())
+        val productId = productIdString.toObjectId()
+
+        if (!user.favoriteProductIds.contains(productId)) {
+            customerRepository.addFavoriteProduct(user.id!!, productId)
+            productRepository.increaseFavoriteCount(productId)
+        }
+    }
+
+    fun removeFavoriteProduct(userId: String, productIdString: String) {
+        val user = customerRepository.getById(userId.toObjectId())
+        val productId = productIdString.toObjectId()
+
+        if (user.favoriteProductIds.contains(productId)) {
+            customerRepository.removeFavoriteProduct(user.id!!, productId)
+            productRepository.decreaseFavoriteCount(productId)
+        }
+    }
+}
+
+fun Customer.toDTO(): CustomerDTO {
+    return CustomerDTO(
+        id = id.toString(),
+        name = name,
+        surname = surname,
+        middleName = middleName,
+        avatar = avatar,
+        phone = phone,
+        birthDay = birthDay,
+        gender = gender, nickname = nickname,
+        email = email,
+        registeredAt = registeredAt
+    )
 }
