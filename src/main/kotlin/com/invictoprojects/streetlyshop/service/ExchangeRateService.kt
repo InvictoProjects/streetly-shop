bed58cf189b47b88a7ee0ddd74d7ee35b8c5f728
Volodymyr Pienskoi <vladymyrpenskoy@gmail.com>
2024-01-06 20:33:17 +0200

    Fix ExchangeRateService

--- src/main/kotlin/com/invictoprojects/streetlyshop/service/ExchangeRateService.kt
+++ src/main/kotlin/com/invictoprojects/streetlyshop/service/ExchangeRateService.kt
@@ -1,13 +1,14 @@
 package com.invictoprojects.streetlyshop.service
 
 import com.invictoprojects.streetlyshop.persistence.domain.model.Currency
+import com.invictoprojects.streetlyshop.persistence.domain.model.product.variant.price.ExchangeRate
 import org.springframework.stereotype.Service
 
 @Service
 class ExchangeRateService(
 ) {
 
-    fun getByCurrency(currency: Currency) {
+    fun getByCurrency(currency: Currency): ExchangeRate {
         TODO()
     }
 
