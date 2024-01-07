b2df3307696b4c70f1542161d9e4b6daf252d8ce
VladyslavHolik <moyfolder@gmail.com>
2024-01-07 02:20:36 +0200

    Add tests for Product Service

---
+++ src/test/kotlin/com/invictoprojects/streetlyshop/service/ProductServiceTest.kt
@@ -0,0 +1,196 @@
+package com.invictoprojects.streetlyshop.service
+
+import com.invictoprojects.streetlyshop.persistence.CategoryRepository
+import com.invictoprojects.streetlyshop.persistence.ProductRepository
+import com.invictoprojects.streetlyshop.persistence.domain.model.*
+import com.invictoprojects.streetlyshop.persistence.domain.model.product.Category
+import com.invictoprojects.streetlyshop.persistence.domain.model.product.Product
+import com.invictoprojects.streetlyshop.persistence.domain.model.product.ProductStatus
+import com.invictoprojects.streetlyshop.service.facade.AuthenticationFacade
+import com.invictoprojects.streetlyshop.web.controller.dto.AttributeDTO
+import com.invictoprojects.streetlyshop.web.controller.request.CreateProductRequest
+import com.invictoprojects.streetlyshop.web.controller.request.UpdateProductRequest
+import com.invictoprojects.streetlyshop.web.exception.InvalidAttributeException
+import com.invictoprojects.streetlyshop.web.exception.ProductNotFoundException
+import com.invictoprojects.streetlyshop.web.exception.UserNotAuthorizedException
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.catchThrowable
+import org.bson.types.ObjectId
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.*
+import org.mockito.BDDMockito.given
+import org.mockito.Mockito.*
+import org.mockito.junit.jupiter.MockitoExtension
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import java.util.*
+
+
+@ExtendWith(MockitoExtension::class)
+internal class ProductServiceTest {
+
+    @Mock
+    lateinit var productRepository: com.invictoprojects.streetlyshop.persistence.ProductRepository
+
+    @Mock
+    lateinit var categoryRepository: com.invictoprojects.streetlyshop.persistence.CategoryRepository
+
+    @Mock
+    lateinit var attributeService: AttributeService
+
+    @Mock
+    lateinit var authenticationFacade: AuthenticationFacade
+
+    @Captor
+    lateinit var productCaptor: ArgumentCaptor<Product>
+
+    @InjectMocks
+    lateinit var productService: ProductService
+
+    @Test
+    fun createProduct_attributeIsNotValid_exceptionIsThrown() {
+        val attribute = AttributeDTO(id = ObjectId().toString(), valueId = ObjectId().toString())
+        val request = CreateProductRequest(
+            categoryId = ObjectId().toString(),
+            attributes = listOf(attribute)
+        )
+
+        given(attributeService.validateAttributes(listOf(attribute))).willThrow(InvalidAttributeException("Error"))
+
+        val throwable = catchThrowable { productService.createProduct(request) }
+
+        assertThat(throwable).isInstanceOf(InvalidAttributeException::class.java)
+
+        verifyNoInteractions(productRepository, categoryRepository, authenticationFacade)
+    }
+
+    @Test
+    fun createProduct_requestIsValid_productIsCreated() {
+        // given
+        val attribute = AttributeDTO(id = ObjectId().toString(), valueId = ObjectId().toString())
+        val categoryId = ObjectId()
+        val request = CreateProductRequest(
+            categoryId = categoryId.toString(),
+            attributes = listOf(attribute)
+        )
+
+        val category = Category(id = categoryId, languageCode = Language.En)
+        given(categoryRepository.getById(categoryId, Language.En)).willReturn(category)
+
+        val currentUserId = ObjectId()
+        val authentication = UsernamePasswordAuthenticationToken(currentUserId.toString(), null)
+        given(authenticationFacade.getAuthentication()).willReturn(authentication)
+
+        given(productRepository.save(any())).willAnswer(AdditionalAnswers.returnsFirstArg<Product>())
+
+        // when
+        val productDTO = productService.createProduct(request)
+
+        // then
+        verify(productRepository).save(capture(productCaptor))
+
+        val actualProduct = productCaptor.value
+        assertThat(actualProduct.categoryId).isEqualTo(categoryId)
+        assertThat(actualProduct.createdBy).isEqualTo(currentUserId)
+        assertThat(actualProduct.attributes).isEqualTo(request.attributes.map { it.toAttribute() })
+
+        assertThat(productDTO).isEqualTo(actualProduct.toDTO())
+    }
+
+    @Test
+    fun getProduct_productExists_productDTOIsReturned() {
+        // given
+        val productId = ObjectId()
+        val product = Product(id = productId, categoryId = ObjectId(), createdBy = ObjectId())
+
+        given(productRepository.getByIdAggregated(productId, Language.En)).willReturn(product)
+
+        // when
+        val productDTO = productService.getProduct(productId.toString(), Language.En)
+
+        // then
+        assertThat(productDTO).isEqualTo(product.toDTO())
+    }
+
+    @Test
+    fun updateProduct_productDoesNotExist_exceptionIsThrown() {
+        // given
+        val productId = ObjectId()
+        val request = UpdateProductRequest(categoryId = ObjectId().toString())
+
+        given(productRepository.getById(productId)).willThrow(ProductNotFoundException("error"))
+
+        // when
+        val throwable = catchThrowable { productService.updateProduct(productId.toString(), request) }
+
+        // then
+        assertThat(throwable).isInstanceOf(ProductNotFoundException::class.java)
+    }
+
+    @Test
+    fun updateProduct_userIsNotAuthorized_exceptionIsThrown() {
+        // given
+        val productId = ObjectId()
+        val request = UpdateProductRequest(categoryId = ObjectId().toString())
+
+        val product = Product(id = productId, categoryId = ObjectId(), createdBy = ObjectId())
+        given(productRepository.getById(productId)).willReturn(product)
+
+        val currentUserId = ObjectId()
+        val authentication = UsernamePasswordAuthenticationToken(currentUserId.toString(), null)
+        given(authenticationFacade.getAuthentication()).willReturn(authentication)
+
+        // when
+        val throwable = catchThrowable { productService.updateProduct(productId.toString(), request) }
+
+        // then
+        assertThat(throwable).isInstanceOf(UserNotAuthorizedException::class.java)
+    }
+
+    @Test
+    fun updateProduct_requestIsValid_productIsUpdated() {
+        // given
+        val productId = ObjectId()
+        val categoryId = ObjectId()
+
+        val attributeId = ObjectId()
+        val attributeValueId = ObjectId()
+        val attributes = listOf(AttributeDTO(id = attributeId.toString(), valueId = attributeValueId.toString()))
+
+        val request = UpdateProductRequest(
+            categoryId = categoryId.toString(),
+            attributes = attributes,
+            productStatus = ProductStatus.ACTIVE
+        )
+
+        val currentUserId = ObjectId()
+        val product = Product(id = productId, categoryId = ObjectId(), createdBy = currentUserId)
+        given(productRepository.getById(productId)).willReturn(product)
+
+        val authentication = UsernamePasswordAuthenticationToken(currentUserId.toString(), null)
+        given(authenticationFacade.getAuthentication()).willReturn(authentication)
+
+        val category = Category(id = categoryId, languageCode = Language.En)
+        given(categoryRepository.getById(categoryId, Language.En)).willReturn(category)
+
+        given(productRepository.save(any())).willAnswer(AdditionalAnswers.returnsFirstArg<Product>())
+
+        // when
+        val productDTO = productService.updateProduct(productId.toString(), request)
+
+        // then
+        verify(productRepository).save(capture(productCaptor))
+
+        val actualProduct = productCaptor.value
+        assertThat(actualProduct.categoryId).isEqualTo(categoryId)
+        assertThat(actualProduct.attributes).hasSize(1)
+        assertThat(actualProduct.attributes[0].id).isEqualTo(attributeId)
+        assertThat(actualProduct.attributes[0].valueId).isEqualTo(attributeValueId)
+        assertThat(actualProduct.status).isEqualTo(ProductStatus.ACTIVE)
+
+        assertThat(productDTO).isEqualTo(actualProduct.toDTO())
+    }
+
+    private fun <T> any(): T = Mockito.any()
+    private fun <T> capture(argumentCaptor: ArgumentCaptor<T>): T = argumentCaptor.capture()
+}
