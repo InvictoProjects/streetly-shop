9f45bbca1bb5f106fa827239efe370151674f11f
Koroliuk <koroliuk.yaroslav@gmail.com>
2024-01-07 16:40:48 +0200

    Refactor attribute services

--- src/main/kotlin/com/invictoprojects/streetlyshop/service/AttributeSearchService.kt
+++ src/main/kotlin/com/invictoprojects/streetlyshop/service/AttributeSearchService.kt
@@ -1,9 +1,9 @@
 package com.invictoprojects.streetlyshop.service
 
 import com.invictoprojects.streetlyshop.persistence.ProductRepository
-import com.invictoprojects.streetlyshop.persistence.domain.model.product.attribute.AttributeSearch
 import com.invictoprojects.streetlyshop.persistence.impl.toObjectId
 import com.invictoprojects.streetlyshop.service.model.AttributeSearchAggregation
+import com.invictoprojects.streetlyshop.util.toDTO
 import com.invictoprojects.streetlyshop.web.controller.dto.AttributeSearchDTO
 import com.invictoprojects.streetlyshop.web.controller.request.AttributeSearchRequest
 import org.springframework.stereotype.Service
@@ -27,10 +27,3 @@ fun AttributeSearchRequest.toAggregation(): AttributeSearchAggregation {
     )
 }
 
-fun AttributeSearch.toDTO(): AttributeSearchDTO {
-    return AttributeSearchDTO(
-        id = id.toString(),
-        definition = definition.toDTO(),
-        values = values.map { it.toDTO() }
-    )
-}
